454dfab9c2d0b507eb7ec683874987518a5ee3ce
KacperCLK <kacper.celak@gmail.com>
2024-08-21 00:56:52 +0200

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,11 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('portfolio')->group(function () {
-    Route::get('/home', [PageController::class, 'home'])->name('home');
-    Route::get('/', function () {
-        return redirect()->route('home');
-    });
-    
-    Route::get('/contact', [PageController::class, 'contact'])->name('contact');
-    Route::get('/skills', [PageController::class, 'skills'])->name('skills');
-    Route::get('/works', [PageController::class, 'works'])->name('works');
+Route::get('/home', [PageController::class, 'home'])->name('home');
+Route::get('/', function () {
+    return redirect()->route('home');
 });
+
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/skills', [PageController::class, 'skills'])->name('skills');
+Route::get('/works', [PageController::class, 'works'])->name('works');
